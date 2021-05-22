dd7827ce9f018f8f61411a0a14c90e3d639daa91
boyvillain <80384067+boyvillain@users.noreply.github.com>
2021-05-22 15:10:10 -0700

    sketch added

---
+++ sketch/sketch.js
@@ -0,0 +1,43 @@
+function setup() {
+createCanvas(600, 600);
+}
+function draw() {
+noStroke()
+background(100, 0, 0);
+//beanie
+fill(100,100,100);
+arc(50,55,60,60,PI,PI);
+ //shirt 
+  fill(0,0,0);
+  ellipse(300,525,300,300);
+  rect(150, 525, 300, 300);
+   fill(57,255,20);
+  text('Planet Punk', 260,500);
+fill(230, 180, 140);
+ellipse(300, 300, 250, 300 );
+//eye
+fill(255,255,255)
+ellipse(240, 250, 60, 40,);
+fill(255,255,255)
+ellipse(360, 250, 60, 40,);
+//blush
+fill(217, 134, 149)
+ellipse(210, 300, 50, 26,);
+fill(217, 134, 149)
+ellipse(390, 300, 50, 26,);
+//nose
+fill (235, 190, 145);
+ellipse (300, 305, 25, 85);
+fill (235, 190, 145);
+ellipse (300, 340, 48, 25);
+//mouth
+  fill(191,112,104);
+  ellipse(330,380,75,30);
+  ellipse(270,380,75,30);
+  fill(225,149,139);
+  arc(300,380,135,60,TWO_PI,PI);
+//beanie2
+fill(110,110,110);
+arc(300, 220, 240, 210, PI, TWO_PI);
+rect(175, 200,250, 50);
+}  
